e7393fda4b3ea34f1ecbd8e112aaa7dabaace060
David Palm <dvdplm@gmail.com>
2020-06-04 17:04:16 +0200

    Add note about CFLAGS="-gz=none"

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 TARGET_NAME=tls-enclave
 # FIXME: using --release to work around linker errors due to "compressed sections"
+# Alternatively, use `CFLAGS="-gz=none"` when building.
 TARGET_DIR=ra-enclave/target/x86_64-fortanix-unknown-sgx/release/examples
 TARGET=$TARGET_DIR/$TARGET_NAME.sgxs
 
